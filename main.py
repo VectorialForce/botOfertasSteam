d52f5ea5bdf736bad0c74e5291ca4c0210c4fa8f
Leo <leonel09-@hotmail.com>
2025-07-11 15:01:00 -0300

    integrar API de twitter para publicar la lista de juegos

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ from Utils.helpers import obtenerTresJuegos
 from twitterBot import *
 
 if __name__ == "__main__":
+    scrapearITAD()
     test()
-    #scrapearITAD()
-    #obtenerTresJuegos(1)
-    #postearJuegosEnTwitter()
